e118325f102b99dad808e912e31b8e204b8e2730
BrunoVigel <brunorosav@gmail.com>
2021-06-10 13:02:43 -0300

    second mobile commit

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 
 var swiper = new Swiper(".caralho", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-      stopOnLastSlide: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    stopOnLastSlide: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  setTimeout(() => {
-    swiper.autoplay.stop()
-  }, 7000);
+setTimeout(() => {
+  swiper.autoplay.stop()
+}, 7000);
+
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const hamburger = document.querySelector(".menu-btn");
+const overlay = document.querySelector(".overlay")
+
+hamburger.addEventListener("click", () => {
+  overlay.classList.toggle("open");
+});
+
+
+
+
+
+
+window.onscroll = function () { myFunction() };
+
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
